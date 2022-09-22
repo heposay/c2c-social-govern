aba2e24a30c65dd73013598f6b7bb0f0b428283a
hepoLove <291960665@qq.com>
2022-09-22 10:23:38 +0800

    feat: 1.添加了spring mvc时间格式转化的bean，解决前端传过来的时间解析成LocalDateTime报错问题

--- c2c-social-govern-mall/src/main/java/com/hepo/c2c/social/govern/mall/config/MvcConfig.java
+++ c2c-social-govern-mall/src/main/java/com/hepo/c2c/social/govern/mall/config/MvcConfig.java
@@ -1,13 +1,23 @@
 package com.hepo.c2c.social.govern.mall.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.hepo.c2c.social.govern.mall.interceptor.LoginInterceptor;
 import com.hepo.c2c.social.govern.mall.interceptor.RefreshTokenInterceptor;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.format.Formatter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 
 /**
  * Spring mvc拦截器配置
@@ -32,5 +42,36 @@ public class MvcConfig implements WebMvcConfigurer {
         //添加token刷新拦截器
         registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
 
+    }
+
+
+
+    // 时间格式化
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+
+    @Bean
+    public Formatter<LocalDateTime> localDateFormatter() {
+
+        return new Formatter<LocalDateTime>() {
+
+            @Override
+            public LocalDateTime parse(String text, Locale locale) throws ParseException {
+                return LocalDateTime.parse(text, FORMATTER);
+            }
+
+            @Override
+            public String print(LocalDateTime object, Locale locale) {
+                return object.format(FORMATTER);
+            }
+        };
+    }
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder
+                // 反序列化
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(FORMATTER))
+                // 序列化
+                .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(FORMATTER));
     }
 }
